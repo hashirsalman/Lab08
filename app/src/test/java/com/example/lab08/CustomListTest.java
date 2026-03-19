25666e9a7da845231d0c361a2e509ec027d058a3
hashirsalman <m.hashirsalman@gmail.com>
2026-03-19 05:39:54 +0500

    Add failing test for countCities

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -21,13 +21,13 @@ public class CustomListTest {
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
 
-        // Verify it was added
+
         assertTrue(list.hasCity(calgary));
 
-        // This will show an error because delete() doesn't exist yet!
+
         list.delete(calgary);
 
-        // Verify it was actually removed
+
         assertFalse(list.hasCity(calgary));
     }
 
@@ -36,10 +36,22 @@ public class CustomListTest {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
 
-        // This will show an error because delete() doesn't exist yet!
+
         assertThrows(IllegalArgumentException.class, () -> {
             list.delete(calgary);
         });
+    }
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+
+
+        assertEquals(0, list.countCities());
+
+
+        list.addCity(calgary);
+        assertEquals(1, list.countCities());
     }
 
 
