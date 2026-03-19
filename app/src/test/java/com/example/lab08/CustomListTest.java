c3a1fe9086fd64160d0c15c5c581acf4fb8fd569
hashirsalman <m.hashirsalman@gmail.com>
2026-03-19 05:14:05 +0500

    Add failing tests for delete

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -15,6 +15,33 @@ public class CustomListTest {
         assertTrue(list.hasCity(calgary));
     }
 
+    @Test
+    public void testDelete() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+
+        // Verify it was added
+        assertTrue(list.hasCity(calgary));
+
+        // This will show an error because delete() doesn't exist yet!
+        list.delete(calgary);
+
+        // Verify it was actually removed
+        assertFalse(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteException() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+
+        // This will show an error because delete() doesn't exist yet!
+        assertThrows(IllegalArgumentException.class, () -> {
+            list.delete(calgary);
+        });
+    }
+
 
 
 }
